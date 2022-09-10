ee0401ff48abfd0b33e8242a624ca70f842ae14d
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-11 00:03:34 +0800

    feat: set up routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import * as ROUTES from "./constants/routers";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path={ROUTES.SIGN_IN}></Route>
+        <Route exact path={ROUTES.SIGN_UP}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
