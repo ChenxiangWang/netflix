5f023cc0111e05c977caee0674a842b32f3ad054
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-11 03:09:31 +0800

    feat: add Header to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import JumbotronContainer from "../containers/jumptron";
 import FooterContainer from "../containers/footer";
 import AccordionContainer from "../containers/accordion";
+import HeaderContainer from "../containers/HeaderContainer";
 export default function Home() {
   return (
     <>
+      <HeaderContainer />
       <JumbotronContainer />
       <FooterContainer />
       <AccordionContainer />
