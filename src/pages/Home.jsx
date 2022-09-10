af1cbfb169c31716867ecff553c9f183c3b189a6
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-11 00:03:07 +0800

    feat: add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import JumbotronContainer from "../containers/jumptron";
+import FooterContainer from "../containers/footer";
+import AccordionContainer from "../containers/accordion";
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+      <AccordionContainer />
+    </>
+  );
+}
