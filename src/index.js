5e49591d3888f373a79a8b211d586e9448b20418
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-11 17:31:36 +0800

    feat: connect to firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./global-style";
 import "normalize.css";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <GlobalStyle />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyle />
+      <App />
+    </FirebaseContext.Provider>
   </>
 );
