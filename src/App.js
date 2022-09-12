64ef1fe764c4c7d8cb059beed371d8d37f7c7d48
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-13 03:17:39 +0800

    feat: init App.js with pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Browse from "./pages/Browse";
+import { IfNotLoggedIn, IfNotAuthenticated } from "./components/helpers/routes";
 import * as ROUTES from "./constants/routers";
+import { useAuthListener } from "./hooks";
 
 function App() {
+  const user = useAuthListener();
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path={ROUTES.HOME} element={<Home />} />
-        <Route exact path={ROUTES.SIGN_IN}></Route>
-        <Route exact path={ROUTES.SIGN_UP}></Route>
+        <Route element={<IfNotLoggedIn user={user} to={ROUTES.BROWSE} />}>
+          <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+        </Route>
+        <Route element={<IfNotAuthenticated user={user} to={ROUTES.HOME} />}>
+          <Route path={ROUTES.BROWSE} element={<Browse />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
