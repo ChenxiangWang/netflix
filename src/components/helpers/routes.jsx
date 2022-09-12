3f4d977ae4eb1f494afcf6ab258e755f4a64344f
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-12 22:56:07 +0800

    fix: obtain user from props

--- src/components/helpers/routes.jsx
+++ src/components/helpers/routes.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-export function IfNotLoggedIn({ to }) {
-  const user = false;
+export function IfNotLoggedIn({ to, user }) {
   if (!user) {
     return <Outlet />;
   } else {
@@ -10,8 +9,7 @@ export function IfNotLoggedIn({ to }) {
   }
 }
 
-export function IfNotAuthenticated({ to }) {
-  const user = false;
+export function IfNotAuthenticated({ to, user }) {
   if (!user) {
     return <Navigate to={to} replace />;
   } else {
