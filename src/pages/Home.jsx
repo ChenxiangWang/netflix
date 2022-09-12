1456580366a225b0e5e04d633c9f40a0ae926d0b
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-12 22:06:04 +0800

    refactor: move Feature out of Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,20 @@ import JumbotronContainer from "../containers/jumptron";
 import FooterContainer from "../containers/footer";
 import AccordionContainer from "../containers/accordion";
 import HeaderContainer from "../containers/HeaderContainer";
+import OptFormContainer from "../containers/OptFormContainer";
+import Feature from "../components/feature";
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited files, TV programs and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptFormContainer />
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FooterContainer />
       <AccordionContainer />
