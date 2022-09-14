4a0ce05d42dae9e5fcc4500808cedbba6be2a14f
jordan.wang <jordan.wang@zoom.us>
2022-09-14 18:25:01 +0800

    feat: add search, nav and dropdown to container

--- src/containers/BrowseContainer.jsx
+++ src/containers/BrowseContainer.jsx
@@ -9,6 +9,7 @@ import * as ROUTES from "../constants/routers";
 export default function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
@@ -23,19 +24,45 @@ export default function BrowseContainer({ slides }) {
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
       <Header src="joker1" dontShowOnSmallViewPort>
         <Header.Frame>
+          {/* left side of the frame bar: include logo and a nav for "filems and series" */}
           <Header.Group>
             <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
             <Header.TextLink>Series</Header.TextLink>
-            <Header.TextLink>Series</Header.TextLink>
-            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
           </Header.Group>
+          {/* right right of the bar*/}
           <Header.Group>
-            <Header.Profile>
-              <Header.Picture src={user.photoURL}></Header.Picture>
-              <Header.Dropdown></Header.Dropdown>
+            {/* search bar at the right side of frame*/}
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+
+            {/* profile block */}
+            <Header.Profile src={user.photoURL}>
+              {/* icon to init trigger the drop down*/}
+              <Header.Picture src={user.photoURL} alt="header-photo" />
+
+              {/* drop down block*/}
+              <Header.Dropdown>
+                {/* TODO: refactor to drop down could be a better way.
+                          however, Header.Group is acting as a Dropdown item
+                */}
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} alt="header-photo" />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
             </Header.Profile>
           </Header.Group>
         </Header.Frame>
+
+        {/* Feature block*/}
         <Header.Feature>
           <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
           <Header.Text>
