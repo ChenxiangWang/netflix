3fbc9b11e2942a4e18fefd80ed67a2dcc459d180
jordan.wang <jordan.wang@zoom.us>
2022-09-14 18:21:17 +0800

    feat: add more sub-componets

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import {
   Background,
@@ -10,6 +10,12 @@ import {
   FeatureCallOut,
   Link,
   Group,
+  Dropdown,
+  Picture,
+  Profile,
+  Search,
+  SearchIcon,
+  SearchInput,
 } from "./style";
 
 export default function Header({ children, bg = true, ...restProps }) {
@@ -54,4 +60,39 @@ Header.TextLink = function ({ children, ...restProps }) {
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
   return <Group {...restProps}>{children} </Group>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive((searchActive) => !searchActive)}
+      >
+        <img src="images/icons/search.png" alt="Search" />
+      </SearchIcon>
+
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+      />
+    </Search>
+  );
 };
