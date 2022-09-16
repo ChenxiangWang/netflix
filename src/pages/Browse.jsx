7ecf17eec097c8d8c41390679e66cc95cd2b2760
jordan.wang <jordan.wang@zoom.us>
2022-09-16 17:55:11 +0800

    feat: add Footer

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { useContent } from "../hooks";
 import selectonFilter from "../utils/selection-filter";
 import BrowseContainer from "../containers/BrowseContainer";
-
+import FooterContainer from "../containers/footer";
 export default function Browse() {
   const { films } = useContent("films");
   const { series } = useContent("series");
   const slides = selectonFilter({ series, films });
 
-  return <BrowseContainer slides={slides} />;
+  return (
+    <>
+      <BrowseContainer slides={slides} />;
+      <FooterContainer />
+    </>
+  );
 }
