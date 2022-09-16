79496d7ea3e63591d674836426c834f1429c3933
jordan.wang <jordan.wang@zoom.us>
2022-09-16 17:55:56 +0800

    feat: add Meta and Feature

--- src/containers/BrowseContainer.jsx
+++ src/containers/BrowseContainer.jsx
@@ -97,13 +97,26 @@ export default function BrowseContainer({ slides }) {
               <Card.Title>{slideItem.title}</Card.Title>
               <Card.Entities>
                 {slideItem.data.map((item) => (
-                  <Card.Item key={item.docId}>
+                  <Card.Item key={item.docId} item={item}>
                     <Card.Image
                       src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
                     />
+
+                    {/* a popover cardition*/}
+                    <Card.Meta>
+                      <Card.SubTitle>{item.title}</Card.SubTitle>
+                      <Card.Text>{item.description}</Card.Text>
+                    </Card.Meta>
                   </Card.Item>
                 ))}
               </Card.Entities>
+              <Card.Feature category={category}>
+                {/* <Player>
+                  <Player.Button />
+                  <Player.Video src={"/videos/bunny.mp4"} />
+                </Player> */}
+                <p>Hi, this player</p>
+              </Card.Feature>
             </Card>
           );
         })}
