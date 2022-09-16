5da644ad85d29d735c3e4359dff737aa98ca1986
jordan.wang <jordan.wang@zoom.us>
2022-09-16 17:58:16 +0800

    feat: init Player page

---
+++ src/components/player/index.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useContext, createContext } from "react";
+import ReactDOM from "react-dom";
+import { Container, Button, Overlay, Inner, Close } from "./style";
+
+export default function Player() {
+  return <div>index</div>;
+}
