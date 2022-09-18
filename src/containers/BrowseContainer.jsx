6e1914d1214ae24e403437acaa776c92c3709767
chenxiang wang <z5083841@student.unsw.edu.au>
2022-09-19 00:41:32 +0800

    feat: enable "search" functionality

--- src/containers/BrowseContainer.jsx
+++ src/containers/BrowseContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import ProfileContainer from "./ProfileContainer";
+import Fuse from "fuse.js";
 import { FirebaseContext } from "../context/firebase";
 import Loading from "../components/loading";
 import Header from "../components/header";
@@ -15,13 +16,25 @@ export default function BrowseContainer({ slides }) {
   const [searchTerm, setSearchTerm] = useState("");
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
-  const slideRows = slides[category];
+  const [slideRows, setSlideRows] = useState(slides[category]);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, [profile.displayName]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
